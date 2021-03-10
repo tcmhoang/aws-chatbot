187af4e72d3b3987ab18f143a077c1907d3e830a
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2021-03-09 23:17:36 -0500

    Hook with DynamoDB and add some function to query

--- lamda-function.py
+++ lamda-function.py
@@ -16,6 +16,10 @@ logger.setLevel(logging.DEBUG)
 MOVIE_TABLE = os.getenv('MOVIE_TABLE', default='DummyMovie')
 ORDER_TABLE = os.getenv('ORDER_TABLE', default='DummyOrder')
 
+# Init DynamoDB Client
+dynamodb = boto3.client("dynamodb")
+
+
 # --- Helpers that build all of the responses ---
 
 
@@ -107,6 +111,86 @@ def build_validation_result(is_valid: bool, violated_slot: str, message_content:
     }
 
 
+""" --- Functions that interact with other services (backend functions) --- """
+
+
+def get_movie_names():
+    """
+    Called to get a list of available movie.
+    """
+    return ['Clarice', 'Wanda Vision']
+
+
+def get_movie_id(movie_name: str, theater_name: str):
+    movie_details = dynamodb.query(
+        TableName=MOVIE_TABLE,
+        IndexName='movieName-theaterName-index',
+        KeyConditionExpression='movieName = :mName AND theaterName = :tName',
+        ExpressionAttributeValues={
+            ':mName': {
+                "S": movie_name
+            },
+            ':tName': {
+                "S": theater_name
+            }
+        }
+    )
+    if len(movie_details['Items']) != 0:
+        return parse_int(movie_details['Items'][0]['movieId']['N'])
+
+    return None
+
+
+""" --- Functions that control the bot's behavior (bot intent handler) --- """
+
+
+def i_book_ticket(intent_request):
+    pass
+
+
+def i_movie_theater(intent_request):
+    pass
+
+
+def i_help(intent_request):
+    """
+    Called when the user triggers the Help intent.
+    """
+
+    # Intent fulfillment
+    return close(intent_request['sessionAttributes'],
+                 'Fulfilled',
+                 {'contentType': 'PlainText',
+                  'content': "Hi this is lex, your personal assistant. "
+                             "- Would you like to book movie tickets? "
+                             "- or should I show you a list of available movies for "
+                             "one of the theater?"})
+
+
+""" --- Dispatch intents --- """
+
+
+def dispatch(intent_request):
+    """
+    Called when the user specifies an intent for this bot.
+    """
+
+    logger.debug('dispatch userId={}, intentName={}'.format(
+        intent_request['userId'], intent_request['currentIntent']['name']))
+
+    intent_name = intent_request['currentIntent']['name']
+
+    # Dispatch to your bot's intent handlers
+    if intent_name == 'BookTickets':
+        return i_book_ticket(intent_request)
+    elif intent_name == 'GetMovieTheater':
+        return i_movie_theater(intent_request)
+    elif intent_name == 'Help':
+        return i_help(intent_request)
+
+    raise Exception('Intent with name ' + intent_name + ' not supported')
+
+
 """ --- Main handler --- """
 
 
@@ -118,6 +202,6 @@ def lambda_handler(event, context):
     # By default, treat the user request as coming from the Pacific timezone.
     os.environ['TZ'] = 'America/Los_Angeles'
     time.tzset()
-    logger.info(f'Received event: {event} from {event['bot']['name']}')
+    logger.info(f'Received event: {event} from {event["bot"]["name"]}')
 
     return dispatch(event)
